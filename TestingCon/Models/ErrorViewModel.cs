7e1060fe6e84d6e14d5c7d697117450c28d0a665
Prakhar <agrawalprakhar893@gmail.com>
2024-02-08 13:56:44 +0530

    hh

--- TestingCon/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TestingCon.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
