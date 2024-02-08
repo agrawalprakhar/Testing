cc962bea4b74b91a69ff9d69a4336dc09e553806
Prakhar <agrawalprakhar893@gmail.com>
2024-02-08 14:06:11 +0530

    jj

--- TestingCon/Controllers/HomeController.cs
+++ TestingCon/Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using TestingCon.Models;
 
 namespace TestingCon.Controllers
 {
@@ -23,10 +22,7 @@ namespace TestingCon.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
+        
     }
 }
